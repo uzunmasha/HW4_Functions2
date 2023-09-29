010a4561e6b000d54818fdf80a30c64e7f291ea3
Maria Uzun <uzunmasha@gmail.com>
2023-09-29 19:20:35 +0300

    Add aa_count function

--- HW4_Uzun/AAmigo.py
+++ HW4_Uzun/AAmigo.py
@@ -19,3 +19,26 @@ def aa_substring(seq: str):
         subst = sequences.find(substring)
         results.append(subst)
     return results
+
+
+def aa_count(seq: str):
+    """
+
+    Finds how many times a particular sequence(s) occurs in the original one.
+    Takes a string of amino acids and a substring, which should be counted.
+    Returns the count of searched amino acids.
+    Amino acids in the string should be indicated as one-letter symbols.
+
+    """
+    aa_seq = list(seq)
+    aa_seq_upper = []
+    for sequences in aa_seq:
+        upper_case = sequences.upper()
+        aa_seq_upper.append(upper_case)
+    amino_acids = aa_seq_upper[:-1]
+    substring = aa_seq_upper[-1]
+    results = []
+    for sequences in amino_acids:
+        amino_acid_count = sequences.count(substring)
+        results.append(amino_acid_count)
+    return results
