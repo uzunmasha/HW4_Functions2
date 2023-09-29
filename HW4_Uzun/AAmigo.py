b9dce5b9636a8eb18bad989a90e7fb62254d5e2c
Maria Uzun <uzunmasha@gmail.com>
2023-09-29 19:15:22 +0300

    Add aa_substring function

--- HW4_Uzun/AAmigo.py
+++ HW4_Uzun/AAmigo.py
@@ -1,1 +1,21 @@
-Add your code here
+def aa_substring(seq: str):
+    """
+
+    Searches for a substring of amino acids in the entire amino acid sequence.
+    Takes a string of amino acids and a substring, which should be found.
+    Returns the position where the searched one was found for the first time.
+    Amino acids in the string should be indicated as one-letter symbols.
+
+    """
+    aa_seq = list(seq)
+    aa_seq_upper = []
+    for sequences in aa_seq:
+        upper_case = sequences.upper()
+        aa_seq_upper.append(upper_case)
+    amino_acids = aa_seq_upper[:-1]
+    substring = aa_seq_upper[-1]
+    results = []
+    for sequences in amino_acids:
+        subst = sequences.find(substring)
+        results.append(subst)
+    return results
