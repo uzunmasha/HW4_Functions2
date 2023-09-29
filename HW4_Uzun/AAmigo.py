f32cf2bffe47d718dbe885096119f357f076e282
zhurkv <zhur_kv@mail.ru>
2023-09-29 16:57:41 +0300

    Add protein_length function

--- HW4_Uzun/AAmigo.py
+++ HW4_Uzun/AAmigo.py
@@ -1,1 +1,15 @@
 Add your code here
+def protein_length(*seqs: str):
+    """
+
+    Calculate the length (number of amino acids) of a protein.
+    Takes a string of amino acids, returns the number.
+    Amino acids in the string should be indicated as one-letter symbols.
+
+    """
+    lengths = []
+
+    for seq in seqs:
+        lengths.append(len(seq))
+
+    return lengths
