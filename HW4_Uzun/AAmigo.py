a6a3033e2587995e295684f709c49fcd90946493
zhurkv <zhur_kv@mail.ru>
2023-09-29 17:01:48 +0300

    Add     def essential_amino_acids function

--- HW4_Uzun/AAmigo.py
+++ HW4_Uzun/AAmigo.py
@@ -13,3 +13,24 @@ def protein_length(*seqs: str):
         lengths.append(len(seq))
 
     return lengths
+
+
+    def essential_amino_acids(*seqs: str):
+    """
+
+    Calculate the number of essential amino acids based on its amino acids sequence.
+    Takes a string of amino acids, returns only the essential amino acids.
+    Amino acids in the string should be indicated as one-letter symbols.
+
+    """
+    eaa_dictionary = ['H', 'I', 'K', 'L', 'M', 'F', 'T', 'W', 'V', 'h', 'i', 'k', 'l', 'm', 'f', 't', 'w', 'v']
+    eaa_list = []
+
+    for seq in seqs:
+        eaa_seq = []
+        for amino_acid in seq:
+            if amino_acid in eaa_dictionary:
+                eaa_seq.append(amino_acid)
+        eaa_list.append(eaa_seq)
+
+    return eaa_list
