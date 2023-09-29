c70c0b5492624a9e43984d066c5f4e123f885680
icalledmyselfmoon <88886578+icalledmyselfmoon@users.noreply.github.com>
2023-09-29 19:15:13 +0300

    Add function protein_mass to AAmigo.py

--- HW4_Uzun/AAmigo.py
+++ HW4_Uzun/AAmigo.py
@@ -1,1 +1,17 @@
-Add your code here
+def protein_mass(seq: str):
+    """
+
+    Calculate the mass (Da) of a protein based on its amino acids sequence.
+    Takes a string of amino acids, returns the molecular weight in Da.
+    Amino acids in the string should be indicated as one-letter symbols.
+
+    """
+    aa_seq = list(seq)
+    mass_dictionary = dict({'A': 89, 'R': 174, 'N': 132, 'D': 133, 'C': 121, 'Q': 146, 'E': 147, 'Z': 147,
+                            'G': 75, 'H': 155, 'I': 131, 'L': 131, 'K': 146, 'M': 149, 'F': 165, 'P': 115, 'S': 105,
+                            'T': 119, 'W': 204, 'Y': 181, 'V': 117})
+    mass = 0
+    for aa in aa_seq:
+        mass += mass_dictionary[aa]
+
+    return mass
