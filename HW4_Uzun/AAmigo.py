d6e423e0005bc3d5811bae9d19e1177554074d78
Maria Uzun <uzunmasha@gmail.com>
2023-09-29 19:26:45 +0300

    Add main function aa_tools

--- HW4_Uzun/AAmigo.py
+++ HW4_Uzun/AAmigo.py
@@ -42,3 +42,60 @@ def aa_count(seq: str):
         amino_acid_count = sequences.count(substring)
         results.append(amino_acid_count)
     return results
+
+
+def aa_tools(*args):
+    """
+
+    Main function for amino acid sequences processing.
+    Parameters: *args (str) - amino acid sequences and operation.
+    Returns: List of results or None if non-amino acid chars found.
+
+    """
+    seq = args[:-1]
+    operation = args[-1]
+    non_aa_chars = set('BJOUXbjoux')
+    contains_non_aa = False
+
+    for sequence in seq:
+        contains_non_aa = False
+        for amino_acid in sequence:
+            if amino_acid in non_aa_chars:
+                contains_non_aa = True
+                break
+        if contains_non_aa:
+            break
+    if contains_non_aa:
+        return None
+
+    results = []
+
+    for sequence in seq:
+        if operation == "protein_mass":
+            result = protein_mass(sequence)
+            results.append(result)
+
+        elif operation == "aa_profile":
+            result = aa_profile(sequence)
+            results.append(result)
+
+        if operation == "aa_substring":
+            result = aa_substring(seq)
+            return result
+
+        if operation == "aa_count":
+            result = aa_count(seq)
+            return result
+
+        if operation == "protein_length":
+            result = protein_length(sequence)
+            results.append(result)
+
+        if operation == "essential_amino_acids":
+            result = essential_amino_acids(sequence)
+            results.append(result)
+
+    return results
+
+
+aa_tools()
